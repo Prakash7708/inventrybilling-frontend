ee30182c47e30ea6acc841094877c4dd1d7b7d2f
Prakash7708 <bprakash2902@gmail.com>
2022-11-25 21:25:22 +0530

    render-server

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 let instance=axios.create({
     // baseURL:"http://localhost:3001"
-     baseURL:"https://inventrybilling-backend.herokuapp.com/"
+    // baseURL:"https://inventrybilling-backend.herokuapp.com/"
+    //New App
+    baseURL:"https://inventry-server.onrender.com"
 })
 
 export default instance;
